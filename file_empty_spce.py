7fbde7475a3ba77cafaca504c1ee9cb682eca86e
Komalcodes-pixel <psarju414@gmail.com>
2026-01-02 20:35:45 +0530

    Add files via upload

---
+++ file_empty_spce.py
@@ -0,0 +1,12 @@
+#Prgr to remove white spaces
+with open("/data/user/0/com.markodevcic.python_code_pad/cache/file_picker/1767348900114/genrators.py","r") as r:
+    t = r.readlines()
+with open("/data/user/0/com.markodevcic.python_code_pad/cache/file_picker/1767348900114/genrators.py","w") as r:
+    for i in t:
+        if i.strip():
+            r.write(i)
+            
+    
+     
+  
+    
